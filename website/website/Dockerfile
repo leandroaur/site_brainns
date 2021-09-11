9261760204ab4cd78e6902e1987e310aad95a76a
root <root@APUS.localdomain>
2021-09-11 12:37:35 -0300

    update website

---
+++ website/website/Dockerfile
@@ -0,0 +1,6 @@
+FROM nginx:alpine
+
+COPY website /website
+COPY nginx.conf /etc/nginx/nginx.conf
+
+EXPOSE 80
---
+++ website/website/_css/BookingController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExploreCalifornia.WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Book")]
+        public IActionResult Book()
+        {
+            var tourname = Request.Form["tourname"];
+            var name = Request.Form["name"];
+            var email= Request.Form["email"];
+            var needsTransport = Request.Form["transport"] == "on";
+
+            // Send messages here...
+
+            return Redirect($"/BookingConfirmed?tourname={tourname}&name={name}&email={email}");
+        }
+
+        [HttpPost]
+        [Route("Cancel")]
+        public IActionResult Cancel()
+        {
+            var tourname = Request.Form["tourname"];
+            var name = Request.Form["name"];
+            var email = Request.Form["email"];
+            var cancelReason = Request.Form["reason"];
+
+            // Send cancel message here
+
+            return Redirect($"/BookingCanceled?tourname={tourname}&name={name}");
+        }
+    }
+}
